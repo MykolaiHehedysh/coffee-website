344d8c567e0dffc9d4d53684bca14989e9a26608
MykolaiHehedysh <mikolagegedis04@gmail.com>
2026-03-12 21:44:43 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,32 @@
+const menuOpenButton = document.querySelector("#menu-open-button");
+const menuCloseButton = document.querySelector("#menu-close-button");
+
+menuOpenButton.addEventListener("click", () => {
+  document.body.classList.toggle("show-mobile-menu");
+});
+
+menuCloseButton.addEventListener("click", () => {
+  document.body.classList.toggle("show-mobile-menu");
+});
+
+const swiper = new Swiper(".slider-wrapper", {
+  // Optional parameters
+  direction: "horizontal",
+  loop: true,
+
+  // If we need pagination
+  pagination: {
+    el: ".swiper-pagination",
+  },
+
+  // Navigation arrows
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+
+  // And if we need scrollbar
+  scrollbar: {
+    el: ".swiper-scrollbar",
+  },
+});
